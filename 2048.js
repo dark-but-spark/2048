319d0305e616383b14d195757519d1b0ab415b50
chen jiyan <3036789610@qq.com>
2024-11-30 21:56:32 +0800

    new file:   2048.css 	modified:   2048.html 	new file:   2048.js

---
+++ 2048.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const grid=document.querySelector('.grid');
+    const size=4;
+    let board=[];
+    let cnt=0;
+    const cntElem= document.getElementById("current-score");
+
+    let mx= localStorage.getItem("2048-highScore")|| 0;
+    const mxElem=document.getElementById("high-score");
+    mxElem.textContent=mx;
+
+    const GGElem= document.getElementById("game-over");
+
+    function update(value)
+    {
+        cnt+=value;
+        cntElem.textContent=cnt;
+        if(cnt>mx)
+        {
+            mx=cnt;
+            mxElem.textContent= mx;
+            localStorage.setItem('2048-highScore',mx);
+        }
+    }
+
+
+    function restart()
+    {
+        cnt=0;
+        cntElem="0";
+        GGElem.style.diaplay='none';
+        init();
+    }
+
+    function init()
+    {
+        board=[...Array(size)].map(e => Array(size).fill(0));
+        place();
+        place();
+        render();
+    }
+
+    function render()
+    {
+        for(let i=0;i<size;i++)
+        {
+            for(let j=0;jZ<size;j++)
+            {
+                const cell=document.querySelector('[data-row="${i}"][data-col="${j}"]');
+                const preValue= cell.dataset.value;
+                const cntValue= board[i][j];
+                if(cntValue!=0)
+                {
+                    cell.dataset.value=cntValue;
+                    cell.textContent=cntValue;
+                    
+                }
+            }
+        }
+    }
+
+})
