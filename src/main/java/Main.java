231fc674510d4924eeb04e253549926dc3c9141d
chen jiyan <3036789610@qq.com>
2025-03-13 16:14:03 +0800

    add BasicScene class for game menu and game over functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Hello, JavaFX!");
-        StackPane root = new StackPane();
-        root.getChildren().add(label);
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setTitle("Hello JavaFX");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        System.out.println("Starting Game!");
+        BasicScene.Menu(primaryStage);
     }
     public static void main(String[] args) {
         launch(args);
